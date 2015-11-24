93a31dba24552bfb9b0dccab80e83a1e0b6cf118
M. Sonntag <michael.p.sonntag@gmail.com>
2015-11-24 18:12:24 +0100

    prototype: test hashing

--- src/main/java/org/g_node/App.java
+++ src/main/java/org/g_node/App.java
@@ -21,8 +21,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import javax.xml.bind.DatatypeConverter;
+
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.log4j.Logger;
@@ -105,6 +112,8 @@ public class App {
         printQuery(addModel);
 
         constructQuery(addModel);
+
+        testHashing();
     }
 
     /**
@@ -228,4 +237,62 @@ public class App {
         }
     }
 
+    /**
+     * Prototype method - test various hashing mechanisms.
+     */
+    private static void testHashing() {
+        final String hashMe = "hurrastring";
+        final String hashMeResult = "41bc1af4a1782d40a4d3c0fe5396f57d4201f02b1e232e330ea430a642f3d4d0";
+
+        // test hashing - java built in
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] byteDigest = md.digest(hashMe.getBytes("UTF-8"));
+
+            final String sha256hexMD = DatatypeConverter.printHexBinary(byteDigest)
+                                            .toLowerCase(Locale.ENGLISH);
+            final Boolean corrHashMD = hashMeResult.equals(sha256hexMD);
+
+            System.out.println(
+                    String.join("", "MessageDigest sha256: ", sha256hexMD,
+                            " (correct hash: ", corrHashMD.toString(), ")")
+            );
+
+            md = MessageDigest.getInstance("SHA");
+            byteDigest = md.digest(hashMe.getBytes("UTF-8"));
+
+            final String sha1hexMD = DatatypeConverter.printHexBinary(byteDigest).toLowerCase(Locale.ENGLISH);
+
+            System.out.println(
+                    String.join("", "MessageDigest sha-1: ", sha1hexMD)
+            );
+
+            md = MessageDigest.getInstance("SHA-224");
+            byteDigest = md.digest(hashMe.getBytes("UTF-8"));
+
+            final String sha224hexMD = DatatypeConverter.printHexBinary(byteDigest)
+                                            .toLowerCase(Locale.ENGLISH);
+
+            System.out.println(
+                    String.join("", "MessageDigest sha-224: ", sha224hexMD)
+            );
+
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        // hashing apache common codec
+        final String sha256hex = DigestUtils.sha256Hex(hashMe);
+        final Boolean corrHash = hashMeResult.equals(sha256hex);
+        System.out.println(
+                String.join("", "Apache common sha256: ", sha256hex,
+                        " (correct hash: ", corrHash.toString(), ")"));
+
+        final String sha1hex = DigestUtils.shaHex(hashMe);
+        System.out.println(String.join("", "Apache common sha-1: ", sha1hex));
+
+        final String sha512hex = DigestUtils.sha512Hex(hashMe);
+        System.out.println(String.join("", "Apache common sha-512: ", sha512hex));
+    }
 }
