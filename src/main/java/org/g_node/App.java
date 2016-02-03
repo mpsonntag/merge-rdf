e0cc16991d684209299bf47e4f3607b3a97dd82c
M. Sonntag <michael.p.sonntag@gmail.com>
2016-02-03 15:56:15 +0100

    cleanup ModelUtils

--- src/main/java/org/g_node/App.java
+++ src/main/java/org/g_node/App.java
@@ -17,7 +17,6 @@ import java.time.format.DateTimeFormatter;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.log4j.Logger;
-import org.g_node.srv.AppUtils;
 import org.g_node.srv.ModelUtils;
 
 /**
@@ -51,7 +50,6 @@ public class App {
         final RDFFormat outFormat = RDFFormat.TURTLE_PRETTY;
 
         final String mainPath = "/home/msonntag/work/spielwiese/KayRDF/";
-        //final String mainPath = "D:\\Software\\Crawler\\";
 
         final String mainFileName = "rdfTestFiles/LKT_merge_test_01a.ttl";
         final String addFileName = "rdfTestFiles/LKT_merge_test_01b.ttl";
@@ -68,93 +66,10 @@ public class App {
         // Can be a problem, if the addModel has additional prefixes.
         mergeModel.setNsPrefixes(mainModel.getNsPrefixMap());
 
-        // to merge stuff:
-        // 1) define main model and add model
-        // 2) iterate through all resources of the add model and check if they are contained within the main model
-        // 3) if a resource is contained within the main model
-        // 3.1) check if the data properties of the add model are contained within the resource of the main model;
-        // if that is the case remove the corresponding data property from the main model.
-        // 3.2) check if the add model and the main model contain a blank node, if yes remove the blank node from the
-        // main model.
-        // 3.3) in theory check, if the main model and the add model both contain object properties, if yes, check
-        // if there is an existing ontology and check if the corresponding class of the object property is allowed
-        // to contain more than one. if this is not the case, remove the object property of the main model. This might
-        // lead to the interesting case of a disconnected resource in the main model, if the corresponding resource
-        // is not further linked by other object properties. In this case removing of this resource might be required.
-
-        // It seems there is a problem with listing Resources, if a Resource is a top level node.
-        // Top level node is a node that has a URI, references other nodes, but is not referenced
-        // by other nodes. In this case, Apache Jena will not list it as a Resource.
-        // In the use case this is true for the Project Resource which is not referenced by any other node.
         mergeModel = ModelUtils.removePropertiesFromModel(addModel, mainModel, true);
 
         mergeModel.add(addModel);
         ModelUtils.saveModelToFile(mergeModel, outFileName, outFormat);
-
-        final boolean isActive = false;
-        if (isActive) {
-            testRemoveAnonNodes(mainModel, addModel, mergeModel, outFileName, outFormat);
-            testIntersect(mainModel, addModel, mainPath, outFormat);
-            testDiff(mainModel, addModel, mainPath, outFormat);
-            queryResources(addModel);
-            AppUtils.testHashing();
-        }
-    }
-
-    /**
-     * Wrapper for prototype query methods.
-     * @param currModel RDF model to query.
-     */
-    private static void queryResources(final Model currModel) {
-        ModelUtils.walkResources(currModel);
-        ModelUtils.constructQuery(currModel);
-    }
-
-    /**
-     * Wrapper for prototype remove anonymous nodes methods.
-     * @param mainModel Model containing RDF database information.
-     * @param addModel Model containing RDF to add to the database model.
-     * @param mergeModel Empty model in which to merge DB and add model.
-     * @param outFileName Path and filename to save the output file to.
-     * @param outFormat RDFFormat in which the output file is saved as.
-     */
-    private static void testRemoveAnonNodes(final Model mainModel, final Model addModel, final Model mergeModel,
-                                            final String outFileName, final RDFFormat outFormat) {
-        // merge and save models
-        mergeModel.add(mainModel);
-        mergeModel.add(addModel);
-
-        ModelUtils.removeDuplicateAnonNodes(mergeModel);
-        ModelUtils.saveModelToFile(mergeModel, outFileName, outFormat);
-    }
-
-    /**
-     * Wrapper to test Jena Model intersect.
-     * @param mainModel Model containing RDF database information.
-     * @param addModel Model containing RDF to add to the database model.
-     * @param mainPath Path to save the output file to.
-     * @param outFormat RDFFormat in which the output file is saved as.
-     */
-    private static void testIntersect(final Model mainModel, final Model addModel,
-                                      final String mainPath, final RDFFormat outFormat) {
-        // determine intersection and difference of the two models.
-        final Model intersectModel = mainModel.intersection(addModel);
-        final String intersectOutFile = String.join("", mainPath, "test_insersect_out.ttl");
-        ModelUtils.saveModelToFile(intersectModel, intersectOutFile, outFormat);
-    }
-
-    /**
-     * Wrapper to test Jena Model diff.
-     * @param mainModel Model containing RDF database information.
-     * @param addModel Model containing RDF to add to the database model.
-     * @param mainPath Path to save the output file to.
-     * @param outFormat RDFFormat in which the output file is saved as.
-     */
-    private static void testDiff(final Model mainModel, final Model addModel,
-                                 final String mainPath, final RDFFormat outFormat) {
-        final Model diffModel = mainModel.difference(addModel);
-        final String diffOutFile = String.join("", mainPath, "test_diff_out.ttl");
-        ModelUtils.saveModelToFile(diffModel, diffOutFile, outFormat);
     }
 
 }
--- src/main/java/org/g_node/srv/ModelUtils.java
+++ src/main/java/org/g_node/srv/ModelUtils.java
@@ -11,20 +11,13 @@
 package org.g_node.srv;
 
 import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.NodeIterator;
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.RDFNode;
-import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
-import com.hp.hpl.jena.vocabulary.RDF;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 
@@ -57,204 +50,44 @@ public final class ModelUtils {
     }
 
     /**
-     * Prototype method - walk through existing {@link Resource}s of a model, print the content, check for Resources
-     * identical by URI with another model and remove all {@link Property} and anonymous Nodes
-     * from these identical resources of the second model.
-     * @param addModel RDF {@link Model} from which the Resources are printed and checked for in the removeFromModel.
+     * Walk through existing {@link Resource}s of an input RDF {@link Model}, check for Resources
+     * identical by URI with another model and remove all Property and anonymous RDFNodes
+     * from these identical Resources of the second model.
+     * @param inModel RDF {@link Model} from which the Resources are printed and checked for in the removeFromModel.
      * @param removeFromModel RDF {@link Model} from which the resources are removed if identical with resources
-     *                  in the addModel.
-     * @param removeAnonNodes If true, anonymous nodes that are referenced by properties
+     *                  in the inModel.
+     * @param removeAnonNodes If true, anonymous RDFNodes that are referenced by properties
      *                        that are to be removed, are removed as well.
      * @return The {@link Model} removeFromModel from which all Properties and anonymous Nodes
-     * of {@link Model} addModel identical by URI have been removed.
+     * of {@link Model} inModel identical by URI have been removed.
      */
-    public static Model removePropertiesFromModel(final Model addModel, final Model removeFromModel,
+    public static Model removePropertiesFromModel(final Model inModel, final Model removeFromModel,
                                                   final boolean removeAnonNodes) {
-        // TODO check how filters work and filter for o.isURIResource() before doing the .forEachRemaining()
-        addModel.listObjects().forEachRemaining(o -> {
+        inModel.listObjects().forEachRemaining(o -> {
                 if (o.isURIResource()
                         && o.asResource().listProperties().hasNext()
                         && removeFromModel.containsResource(o.asResource())) {
-                    System.out.println(String.join("", "[DEBUG] URI Res: ", o.toString()));
-                    System.out.println("[DEBUG] Curr res is contained in the main model");
-                    final Resource checkRemoveProps = removeFromModel.getResource(o.asResource().getURI());
-                    final List<Statement> mainProps = checkRemoveProps.listProperties().toList();
-                    mainProps.forEach(c -> System.out.println(String.join("", "[DEBUG] ", c.toString())));
-                    final List<Statement> addProps = o.asResource().listProperties().toList();
-                    System.out.println("[DEBUG] \n");
-                    addProps.forEach(c -> System.out.println(String.join("", "[DEBUG] ", c.toString())));
-                    // Problem: Cannot be identical, because the object properties can differ.
-                    System.out.println(
-                            String.join("",
-                                    "[DEBUG] CurrRes: ", o.asResource().getURI(),
-                                    " mainProps length: ", Integer.toString(mainProps.size()),
-                                    " addProps length: ", Integer.toString(addProps.size()),
-                                    " identical: ", Boolean.toString(mainProps.containsAll(addProps)))
-                    );
-
-                    // Remove anonymous nodes of the current Resource
+                    final Resource resRemoveProps = removeFromModel.getResource(o.asResource().getURI());
                     if (removeAnonNodes) {
-                        removeAnonProperties(checkRemoveProps.listProperties());
+                        removeAnonProperties(resRemoveProps.listProperties());
                     }
-
-                    checkRemoveProps.removeProperties();
+                    resRemoveProps.removeProperties();
                 }
             });
-
-        System.out.println("[DEBUG]\n[DEBUG] List remaining objects:\n");
-        removeFromModel.listObjects().forEachRemaining(
-                o -> System.out.println(String.join("", "[DEBUG] ", o.toString())));
-
         return removeFromModel;
     }
 
     /**
-     * Prototype method - check if a statement has an anonymous node as
-     * an RDF Object and to remove all properties of such an anonymous node
-     * from the corresponding model.
-     * @param checkAnon Iterator containing a list of statements.
+     * Check if a statement has an anonymous RDFNode as
+     * an RDF Object and remove all properties of such an anonymous RDFNode
+     * from the containing model.
+     * @param checkAnon This {@link StmtIterator} contains a list of statements.
      */
     private static void removeAnonProperties(final StmtIterator checkAnon) {
         while (checkAnon.hasNext()) {
             final Statement currStmt = checkAnon.nextStatement();
             if (currStmt.getObject().isAnon()) {
-                System.out.println(String.join("", "[DEBUG] Current property |",
-                        currStmt.getPredicate().getLocalName(),
-                        "| of resource ",
-                        currStmt.getSubject().getURI(),
-                        " is anonNode: ",
-                        currStmt.getObject().toString()));
-
-                final Resource currAnonRes = currStmt.getObject().asResource();
-
-                System.out.println(
-                        String.join("", "[DEBUG] ", currAnonRes.toString(),
-                                " has properties: ",
-                                Integer.toString(currAnonRes.listProperties().toList().size()))
-                );
-
-                currAnonRes.removeProperties();
-            }
-        }
-    }
-
-    /**
-     * Prototype method - walk through existing resources of a model and print the content.
-     * @param currModel RDF {@link Model} from which the resources are printed.
-     */
-    public static void walkResources(final Model currModel) {
-        currModel.listObjects().forEachRemaining(o -> {
-                if (o.isURIResource() && o.asResource().listProperties().hasNext()) {
-                    System.out.println(
-                            String.join("",
-                                    "URI Res: ", o.toString(), " (",
-                                    o.asResource().getProperty(RDF.type).getObject().toString(), ")")
-                    );
-
-                    o.asResource().listProperties().forEachRemaining(c -> {
-                            if (c.getObject().isLiteral()) {
-                                System.out.println(
-                                        String.join("",
-                                                "\t has Literal: ", c.getPredicate().toString(),
-                                                " ", c.getObject().toString())
-                                );
-
-                            } else {
-                                System.out.println(
-                                        String.join("",
-                                                "\t has Resource: ", c.getPredicate().toString(),
-                                                " ", c.getObject().toString())
-                                );
-                            }
-                        });
-                } else if (o.isResource() && o.isAnon()) {
-                    System.out.println(String.join("", "Anon Resource: ", o.toString()));
-                }
-            });
-    }
-
-    /**
-     * Prototype method - find duplicate anon nodes and remove the duplicates from the model.
-     * @param mergeModel Model that's supposed to cleaned from duplicate blank nodes.
-     */
-    public static void removeDuplicateAnonNodes(final Model mergeModel) {
-        // Handling just the hasWeight anonNodes for now to see how to deal with duplicate
-        // anonymous nodes.
-        final Property hasWeight = ResourceFactory
-                .createProperty("https://github.com/G-Node/neuro-ontology/", "hasWeight");
-
-        final ResIterator getSubjLogEntries = mergeModel.listResourcesWithProperty(hasWeight);
-        getSubjLogEntries.forEachRemaining(c -> {
-                System.out.println(c.asResource().getURI());
-                if (c.listProperties(hasWeight).toList().size() > 1) {
-                    //Set<Resource> removeUs = new HashSet<>();
-
-                    final List<Statement> currBNL = c.listProperties(hasWeight).toList();
-                    int idx = 0;
-                    while (idx < c.listProperties(hasWeight).toList().size()) {
-                        //Resource currRes = currBNL.get(idx).getObject().asResource();
-                        System.out.println(currBNL.get(idx).getSubject().getLocalName());
-                        System.out.println(currBNL.get(idx).getPredicate().getLocalName());
-                        System.out.println(currBNL.get(idx).getObject().asResource().getId());
-                        idx = idx + 1;
-                    }
-                    /*
-                    // layer SubjectLogEntry
-                    final List<Statement> l = c.listProperties(hasWeight).toList();
-                    // check if this somehow works with int streams as well.
-                    for (Statement s : l) {
-                        // layer Anonymous node
-                        System.out.println(
-                                s.getSubject().getLocalName()
-                        );
-                        System.out.println(
-                                s.getObject().asResource().listProperties().toList()
-                        );
-                    }
-
-                    //System.out.println(Integer.toString(c.listProperties(hasWeight).toList().size()));
-                    //c.listProperties(hasWeight).forEachRemaining(
-                        d -> System.out.println(d.getObject().asResource().listProperties().)
-                    );
-                    */
-                }
-            });
-        // do lkt specific stuff here e.g. removing of double blank node entries.
-    }
-
-    /**
-     * Prototype method - construct queries for all resources containing literals.
-     * @param m RDF {@link Model} from which the queries are extracted.
-     */
-    public static void constructQuery(final Model m) {
-
-        final NodeIterator it = m.listObjects();
-        while (it.hasNext()) {
-
-            final RDFNode o = it.next();
-            if (o.isURIResource()
-                    && o.asResource().listProperties().hasNext()
-                    && o.asResource().hasProperty(RDF.type)) {
-
-                String currQ;
-                currQ = "SELECT ?node WHERE {\n";
-                currQ = String.join("", currQ, "\t?node ",
-                        RDF.type.toString(), " ",
-                        o.asResource().getProperty(RDF.type).getObject().toString(),
-                        " .\n");
-
-                final StmtIterator pIt = o.asResource().listProperties();
-                while (pIt.hasNext()) {
-                    final Statement c = pIt.next();
-                    if (c.getObject().isLiteral()) {
-                        currQ = String.join("", currQ, "\t?node ",
-                                c.getPredicate().toString(), " ",
-                                c.getObject().toString(), " .\n");
-                    }
-                }
-                currQ += String.join("", currQ, "}");
-
-                System.out.println(String.join("", "Query: ", currQ));
+                currStmt.getObject().asResource().removeProperties();
             }
         }
     }
