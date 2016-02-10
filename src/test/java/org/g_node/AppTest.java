7dd121031874453685df053d1d5fabcf1e2244d0
M. Sonntag <michael.p.sonntag@gmail.com>
2016-02-10 11:40:42 +0100

    Main App: add empty CLI arguments test

--- src/test/java/org/g_node/AppTest.java
+++ src/test/java/org/g_node/AppTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, German Neuroinformatics Node (G-Node)
+ * Copyright (c) 2016, German Neuroinformatics Node (G-Node)
  *
  * All rights reserved.
  *
@@ -10,17 +10,65 @@
 
 package org.g_node;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Unit tests for the main App.
+ * Unit tests for the main App. Output and Error streams are redirected
+ * from the console to a different PrintStream and reset after tests are finished
+ * to avoid mixing tool error messages with actual test error messages.
  *
  * @author Michael Sonntag (sonntag@bio.lmu.de)
  */
 public class AppTest {
 
-    @Test
-    public void testMain() throws Exception {
-        System.out.println("Test hurra die Gams");
+    private ByteArrayOutputStream outStream;
+    private PrintStream stdout;
+
+    /**
+     * Redirect Error and Out stream.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+
+        this.stdout = System.out;
+        this.outStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(this.outStream));
+
+        Logger rootLogger = Logger.getRootLogger();
+        rootLogger.setLevel(Level.INFO);
+        rootLogger.addAppender(
+                new ConsoleAppender(
+                        new PatternLayout("[%-5p] %m%n")
+                )
+        );
     }
+
+    /**
+     * Reset Out stream to the console after the tests are done.
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(this.stdout);
+    }
+
+    @Test
+    public void testEmptyMain() throws Exception {
+        final String[] emptyArgs = new String[0];
+        App.main(emptyArgs);
+        assertThat(this.outStream.toString()).contains("[ERROR] No proper merge tool selected!");
+    }
+
 }
