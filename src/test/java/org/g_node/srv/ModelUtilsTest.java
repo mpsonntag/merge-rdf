9cc89af42e0403e68222577467a34fbfdccdc94f
Michael Sonntag <michael.p.sonntag@gmail.com>
2016-02-16 16:35:06 +0100

    Tests: Add ModelUtils test class

---
+++ src/test/java/org/g_node/srv/ModelUtilsTest.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2016, German Neuroinformatics Node (G-Node)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of the BSD License. See
+ * LICENSE file in the root of the Project.
+ */
+
+package org.g_node.srv;
+
+/**
+ * Unit tests for the {@link ModelUtils} class. Output and Error streams are redirected
+ * from the console to a different PrintStream and reset after tests are finished
+ * to avoid mixing tool error messages with actual test error messages.
+ *
+ * @author Michael Sonntag (sonntag@bio.lmu.de)
+ */
+public class ModelUtilsTest {
+
+}
